e0749bcdad8ea31f47591a365970e5026e41fb8e
Nikolai Rodionov <nikolai.rodionov@grandcentrix.net>
2023-05-26 16:13:27 +0200

    Init the validation webhook fot the `DBUser` crd

--- main.go
+++ main.go
@@ -138,6 +138,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DBUser")
 		os.Exit(1)
 	}
+	if err = (&kindarocksv1beta1.DBUser{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "DBUser")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
