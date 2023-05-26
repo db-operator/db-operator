49347f24540fd92ffa82e3b3c968352e8e8d26e5
Nikolai Rodionov <nikolai.rodionov@grandcentrix.net>
2023-05-26 17:50:33 +0200

    WIP: Some work is going on here (this commit should be gone)

--- controllers/dbuser_controller.go
+++ controllers/dbuser_controller.go
@@ -18,40 +18,58 @@ package controllers
 
 import (
 	"context"
+	"time"
 
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	kindarocksv1beta1 "github.com/db-operator/db-operator/api/v1beta1"
+	"github.com/go-logr/logr"
 )
 
 // DBUserReconciler reconciles a DBUser object
 type DBUserReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme   *runtime.Scheme
+	Interval time.Duration
+	Log      logr.Logger
 }
+
+const (
+	dbUserPhaseCreate            = "Creating"
+	dbUserPhaseSecretsTemplating = "SecretsTemplating"
+	dbUserPhaseFinish            = "Finishing"
+	dbUserPhaseReady             = "Ready"
+	dbUserPhaseDelete            = "Deleting"
+)
 
 //+kubebuilder:rbac:groups=kinda.rocks,resources=dbusers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=kinda.rocks,resources=dbusers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kinda.rocks,resources=dbusers/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DBUser object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *DBUserReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	_ = r.Log.WithValues("dbuser", req.NamespacedName)
 
-	// TODO(user): your logic here
+	reconcilePeriod := r.Interval * time.Second
+	reconcileResult := reconcile.Result{RequeueAfter: reconcilePeriod}
 
-	return ctrl.Result{}, nil
+	dbucr := &kindarocksv1beta1.DBUser{}
+	err := r.Get(ctx, req.NamespacedName, dbucr)
+	if err != nil {
+		if k8serrors.IsNotFound(err) {
+			// Requested object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcileResult, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcileResult, err
+	}
+
+	return reconcileResult, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
