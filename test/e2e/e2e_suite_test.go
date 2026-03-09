7463234ad376e06e349ce5e00cb0ebcd74b6e83d
Nikolai Rodionov <iam@allanger.xyz>
2026-03-09 18:25:43 +0100

    Fix test command
    Signed-off-by: Nikolai Rodionov <iam@allanger.xyz>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -1,36 +1,29 @@
 package e2e
 
 import (
-	"db-operator-test/utils"
-	"os/exec"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/stretchr/testify/assert/yaml"
-	corev1 "k8s.io/api/core/v1"
 )
 
-var testData *utils.TestData = &utils.TestData{}
+//var testData *utils.TestData = &utils.TestData{}
 
 func TestE2e(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "E2e Suite")
 }
 
-var _ = BeforeSuite(func() {
-	getCmCmd := exec.Command("kubectl", "--namespace", "default", "get", "configmap", "test-data", "--output", "yaml")
-	cmRaw, err := utils.Run(getCmCmd)
-	Expect(err).ToNot(HaveOccurred())
-
-	cm := &corev1.ConfigMap{}
-	err = yaml.Unmarshal([]byte(cmRaw), cm)
-	Expect(err).ToNot(HaveOccurred())
-
-	err = yaml.Unmarshal([]byte(cm.Data["test_data.yaml"]), testData)
-	Expect(err).ToNot(HaveOccurred())
-})
-
-var dbinstanceWaitReady func() = func() {
-
-}
+// Not needed atm
+//var _ = BeforeSuite(func() {
+//	getCmCmd := exec.Command("kubectl", "--namespace", "default", "get", "configmap", "test-data", "--output", "yaml")
+//	cmRaw, err := utils.Run(getCmCmd)
+//	Expect(err).ToNot(HaveOccurred())
+//
+//	cm := &corev1.ConfigMap{}
+//	err = yaml.Unmarshal([]byte(cmRaw), cm)
+//	Expect(err).ToNot(HaveOccurred())
+//
+//	err = yaml.Unmarshal([]byte(cm.Data["test_data.yaml"]), testData)
+//	Expect(err).ToNot(HaveOccurred())
+//})
